b3b08f8462ae15e153674bd219dde79f3df7aabf
lanhnv <lanhnv4@fe.edu.vn>
2021-10-19 20:25:34 +0700

    Save Change on server

--- src/app/diskdetail/diskdetail.component.ts
+++ src/app/diskdetail/diskdetail.component.ts
@@ -26,6 +26,7 @@ export class DiskdetailComponent implements OnInit {
   prev: string;
   next: string;
   errMess: string;
+  dishcopy: Dish;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
@@ -54,7 +55,7 @@ export class DiskdetailComponent implements OnInit {
     ngOnInit() {
       this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
       this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
        errmess => this.errMess = <any>errmess);
     }
   
@@ -83,6 +84,11 @@ export class DiskdetailComponent implements OnInit {
     this.comment = this.commentForm.value;
     this.comment.date = new Date().toISOString();
     this.dish.comments.push(this.comment);
+    this.dishservice.putDish(this.dishcopy)
+      .subscribe(dish => {
+        this.dish = dish; this.dishcopy = dish;
+      },
+      errmess => { this.dish = null; this.dishcopy = null; this.errMess = <any>errmess; });
     this.commentForm.reset({
       author: '',
       comment: '',
